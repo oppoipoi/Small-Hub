fdc1f6dfa4e2ee8396e1c2f1f192cddaa66aa4d5
Herasium <77067597+oppoipoi@users.noreply.github.com>
2023-01-16 21:25:47 +0100

    Create Main.lua

---
+++ Main.lua
@@ -0,0 +1,22 @@
+local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Rayfield/main/source'))()
+
+local Window = Rayfield:CreateWindow({
+   Name = "Small Hub",
+   LoadingTitle = "Small Hub",
+   LoadingSubtitle = "A Hub for small Games",
+   ConfigurationSaving = {
+      Enabled = true,
+      FolderName = nil, -- Create a custom folder for your hub/game
+      FileName = "Small Hub"
+   },
+   KeySystem = true, -- Set this to true to use our key system
+   KeySettings = {
+      Title = "Small Hub",
+      Subtitle = "Verification System",
+      Note = "A Hub for small games",
+      FileName = "RayfieldKey",
+      SaveKey = true,
+      GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
+      Key = "Small"
+   }
+})
