894a8faac6e6d59e0aa39d0ac25b44fd620002ae
Herasium <77067597+oppoipoi@users.noreply.github.com>
2023-01-16 21:42:21 +0100

    Update ScriptList.lua

--- ScriptList.lua
+++ ScriptList.lua
@@ -1,11 +1,11 @@
- local list = {}
+local list = {}
 
 local scripts = {
   ["Pet Family 2"],
   ["Pet Race Clicker!"]
 }
 function list:add()
-  for i,v in pairs(list) do
+  for i,v in pairs(scripts) do
     local Tab = Window:CreateTab(i, 4483362458) 
   end 
 end
